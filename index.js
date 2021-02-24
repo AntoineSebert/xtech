ff7a71ee22ca9ffe245e2bbef462486ddc4db2fc
Antoine <antoine.sb@orange.fr>
2021-02-24 23:45:48 +0100

    remove sentry check

--- index.js
+++ index.js
@@ -54,10 +54,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 		}
 	});
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-	throw new Error("My first Sentry error!");
-});
-
 // The error handler must be before any other error middleware and after all controllers
 app.use(Sentry.Handlers.errorHandler());
 
