11318b5fcb6c27785d5b726d0ca1dcb4c4846d44
Antoine <antoine.sb@orange.fr>
2021-02-26 13:40:43 +0100

    lint

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const Sentry = require('@sentry/node');
-const Tracing = require("@sentry/tracing");
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
-		rejectUnauthorized: false
-	}
-});
+		rejectUnauthorized: false,
+	},
+})
 
-const app = express();
+const app = express()
 
 Sentry.init({
-	dsn: "https://7cb1483faf514dc2afa659e416647637@o197514.ingest.sentry.io/5651105",
+	dsn: process.env.SENTRY_DSN,
 	integrations: [
 		// enable HTTP calls tracing
 		new Sentry.Integrations.Http({ tracing: true }),
@@ -25,13 +25,13 @@ Sentry.init({
 	// We recommend adjusting this value in production, or using tracesSampler
 	// for finer control
 	tracesSampleRate: 1.0,
-});
+})
 
 // RequestHandler creates a separate execution context using domains, so that every
 // transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.requestHandler())
 // TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.tracingHandler())
 
 app.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
@@ -43,18 +43,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 	/*.post('/singup' => singup) */
 	.get('/db', async (req, res) => {
 		try {
-			const client = await pool.connect();
-			const result = await client.query('SELECT * FROM test_table');
-			const results = { 'results': (result) ? result.rows : null};
-			res.render('pages/db', results );
-			client.release();
+			const client = await pool.connect()
+			const result = await client.query('SELECT * FROM test_table')
+			const results = { results: result ? result.rows : null }
+			res.render('pages/db', results)
+			client.release()
 		} catch (err) {
-			console.error(err);
-			res.send("Error " + err);
+			console.error(err)
+			res.send('Error ' + err)
 		}
-	});
+	})
 
 // The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(Sentry.Handlers.errorHandler())
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
