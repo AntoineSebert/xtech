5c2e6308465045a30b39d4739f614681a04f1965
Antoine <antoine.sb@orange.fr>
2021-03-19 13:36:08 +0100

    sql query builder

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -17,7 +17,7 @@ exports.post_feedback = [
 
 		if (errors.isEmpty()) {
 			try {
-				db.QueryVoid("INSERT INTO feedback VALUES(gen_random_uuid(), now(), ${req.body.content})")
+				db.QueryVoid(sql`INSERT INTO feedback VALUES(gen_random_uuid(), now(), ${req.body.content})`)
 					.then(() => res.render("pages/index")); // replace by success
 			} catch (err) {
 				console.error(err);
