8b0038f866af397e9f5ecc82feaddd23cb6e264e
Antoine <antoine.sb@orange.fr>
2021-03-26 13:10:36 +0100

    query unreviewed feedback upon authentication

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -4,7 +4,16 @@ var session = require('express-session');
 const db = require('../lib/db');
 
 exports.get_feedback = function(req, res) {
-	res.render("pages/feedback", {is_auth: req.oidc.isAuthenticated()});
+	if(req.oidc.isAuthenticated())
+		try {
+			const results = db.QueryResult('SELECT * FROM feedback WHERE NOT reviewed');
+			res.render('pages/feedback', { results: results, is_auth: req.oidc.isAuthenticated() });
+		} catch (err) {
+			console.error(err);
+			res.render("pages/feedback", { s_auth: req.oidc.isAuthenticated(), errors: [{msg: err}] });
+		}
+	else
+		res.render("pages/feedback", {is_auth: req.oidc.isAuthenticated()});
 };
 
 exports.post_feedback = [
