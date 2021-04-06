5db1221b6f1d34417dd8c833ba2f97e64fe37ca2
Antoine <antoine.sb@orange.fr>
2021-04-06 11:48:39 +0200

    remove files

--- public/scripts/landing.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    const menuToogle = document.querySelector('.toggle');
-    const showcase = document.querySelector('.showcase');
-
-    menuToogle.addEventListener('click', () => {
-        menuToogle.classList.toggle('active');
-        showcase.classList.toggle('active');
-    });
-}
