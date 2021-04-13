8cc4821c5ea6f4c9b205ca4226d41be131a9be56
Antoine <antoine.sb@orange.fr>
2021-04-13 22:09:10 +0200

    refactor

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,5 +1,3 @@
 exports.get_dashboard = function(req, res) {
-	const nickname = req.oidc.user.nickname;
-
-	res.render("pages/dashboard", { nickname : nickname });
+	res.render("pages/dashboard", { isAuth: req.oidc.isAuthenticated(), nickname: req.oidc.user.nickname });
 };
