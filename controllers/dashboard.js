18ed0ee30c91246026ae312e8e0bbf97d1c28499
Antoine <antoine.sb@orange.fr>
2021-04-27 22:49:29 +0200

    reorganize dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,20 @@
 const db = require('../lib/db');
 
-exports.get_dashboard = function(req, res) {
-	db.pool
-		.query("SELECT location FROM kitchens ORDER BY location")
-		.then(kitchens => res.render("pages/dashboard", { isAuth: true, user: req.oidc.user, kitchens: kitchens }))
-		.catch(err => {
-			console.error(err.stack);
-			res.render("pages/dashboard", { isAuth: true, user: req.oidc.user, errors: [db.err_msg] });
+exports.get_dashboard = async function(req, res) {
+	try {
+		const kitchens = await db.query("SELECT location FROM kitchens ORDER BY location");
+		const feedbacks = await db.query(
+			`SELECT location, state,
+			(SELECT COUNT(*) from feedback where kitchen = kitchens.location and delivery) as delivery,
+			(SELECT COUNT(*) from feedback where kitchen = kitchens.location and temperature) as temperature
+			FROM kitchens order by temperature desc, delivery desc limit 50;`
+		);
+
+		res.render('pages/dashboard', {
+			isAuth: true, user: req.oidc.user, kitchens: kitchens, feedbacks: feedbacks.rows
 		});
+	} catch (err) {
+		console.log(err.stack);
+		res.render("pages/dashboard", { isAuth: true, user: req.oidc.user, errors: [db.err_msg] });
+	}
 };
