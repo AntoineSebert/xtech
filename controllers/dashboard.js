0994dcfc9c6d8491b2a14a5f365be97fb48c9e3b
Antoine <antoine.sb@orange.fr>
2021-04-29 10:19:16 +0200

    improve query

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,13 +1,18 @@
-const db = require('../lib/db');
+const db = require('../db');
 
 exports.get_dashboard = async function(req, res) {
 	try {
 		const kitchens = await db.query("SELECT location FROM kitchens ORDER BY location");
 		const feedbacks = await db.query(
-			`SELECT location, state,
-			(SELECT COUNT(*) from feedback where kitchen = kitchens.location and delivery) as delivery,
-			(SELECT COUNT(*) from feedback where kitchen = kitchens.location and temperature) as temperature
-			FROM kitchens order by temperature desc, delivery desc limit 50;`
+			`with new_feedback as (SELECT kitchen, delivery, temperature
+                                   from feedback
+                                   WHERE time >= (NOW() - interval '1 month'))
+             SELECT location,
+                    (SELECT COUNT(*) from new_feedback where kitchen = kitchens.location and delivery)    as delivery,
+                    (SELECT COUNT(*) from new_feedback where kitchen = kitchens.location and temperature) as temperature
+             FROM kitchens
+             order by temperature desc, delivery desc
+             limit 50;`
 		);
 
 		res.render('pages/dashboard', {
