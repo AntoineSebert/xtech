5c5e2981250d512d0d57a11800b3d1fa9ad7e582
Antoine <antoine.sb@orange.fr>
2021-04-29 10:18:54 +0200

    delay between sending two feedbacks is minimum 1 hour

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,5 +1,5 @@
 const { body, validationResult } = require('express-validator');
-const db = require('../lib/db');
+const { query, err_msg } = require('../db');
 
 exports.post_feedback = [
 	body('location')
@@ -9,58 +9,70 @@ exports.post_feedback = [
 		.escape(),
 	body('kitchen')
 		.trim()
-		.isLength({ min: 1, max: 255 }) // isIn(kitchens)
+		.isLength({ min: 1, max: 255 })
 		.withMessage('Kitchen empty.')
 		.escape(),
+	//check if at least one is checkbox is ticked
 	body('temperature'),
 	body('delivery'),
 	body('comment')
 		.trim()
 		.escape(),
-	(req, res) => {
-		// check has sent feedback recently
-		// get last feedback sent by user, get time, check if less than 10min
+	async (req, res) => {
+		let errors = [];
 
-		const errors = validationResult(req);
+		await query(`
+			SELECT *
+			FROM feedback
+			WHERE email = '${req.oidc.user.email}' AND time >= (NOW() - INTERVAL '1 hour')
+			ORDER BY time DESC
+		`)
+			.then(result => {
+				if(result.rows.length > 0)
+					errors.push(
+						{ msg : "You already sent feedback less than one hour ago, at: " + result.rows[0].time}
+					);
+			})
+			.catch(err => {
+				console.error(err.stack);
+				errors.push(err.stack);
+			});
 
-		//check if at least one is checkbox is ticked
+		const validationErrors = validationResult(req);
 
-		if (errors.isEmpty())
-			db.pool
-				.query(`
-					INSERT INTO feedback(id, time, comment, kitchen, delivery, temperature, email, location)
-					VALUES(
-						DEFAULT,
-						DEFAULT,
-					    '${req.body.comment}',
-				        '${req.body.kitchen}',
-				        '${typeof req.body.delivery != 'undefined' && req.body.delivery}',
-					    '${typeof req.body.temperature != 'undefined' && req.body.temperature}',
-					    '${req.oidc.user.email}',
-					    '${req.body.location}'
-					)
-				`)
+		if(!validationErrors.isEmpty())
+			errors.concat(validationErrors.array());
+
+		if(errors.length === 0)
+			query(`
+				INSERT INTO feedback(id, time, comment, kitchen, delivery, temperature, email, location)
+				VALUES(
+					DEFAULT,
+					DEFAULT,
+				    '${req.body.comment}',
+			        '${req.body.kitchen}',
+			        '${typeof req.body.delivery != 'undefined' && req.body.delivery}',
+				    '${typeof req.body.temperature != 'undefined' && req.body.temperature}',
+				    '${req.oidc.user.email}',
+				    '${req.body.location}'
+				)
+			`)
 				.then(() => res.redirect("/dashboard#feedback")) // add success operation
 				.catch(err => {
 					console.error(err.stack);
-					// check view
 					res.render(
 						"pages/dashboard",
 						{
 							isAuth: req.oidc.isAuthenticated(),
 							user: req.oidc.user,
-							kitchens: kitchens,
-							errors: [{ msg : db.err_msg}]
+							errors: [{ msg : err_msg}]
 						}
 					);
 				});
-		else {
-			console.error(errors.array());
-			// check view
+		else
 			res.render(
 				"pages/dashboard",
-				{ isAuth: req.oidc.isAuthenticated(), user: req.oidc.user, kitchens: kitchens, errors: errors.array() }
+				{ isAuth: req.oidc.isAuthenticated(), user: req.oidc.user, errors: errors }
 			);
-		}
 	}
 ];
