4628724ba86a37854d22a7199eee22e9ea1bf045
Antoine <antoine.sb@orange.fr>
2021-04-29 10:16:25 +0200

    rename and modify API

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
+module.exports = {
+	query: (text, params) => pool.query(text, params),
+	get_client: () => pool.connect(),
+	err_msg: "An error occurred while retrieving the data from the database",
+};
